d7afd56f9a53de5fbefac59c68cbf69f15a7cf82
Mark Gowen <mark.gowen78@gmail.com>
2022-11-09 15:48:39 -0800

    Dogs appear in list in table

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
+function init() {
 
-})
+    fetch('http://localhost:3000/dogs')
+    .then(res => res.json())
+    .then(function(data) {
+        data.forEach((element) => {
+            const table = document.querySelector("#table-body")
+            const dog = document.createElement("tr")
+            const dogName = document.createElement("td")
+            const dogBreed = document.createElement("td")
+            const dogSex = document.createElement("td")
+            const editBtn = document.createElement("button")
+
+            dogName.textContent = element.name
+            dogBreed.textContent = element.breed
+            dogSex.textContent = element.sex
+            editBtn.textContent = "Edit Dog"
+
+            dog.append(dogName, dogBreed, dogSex, editBtn)
+            table.append(dog)
+        })
+    })
+}
+
+init()
