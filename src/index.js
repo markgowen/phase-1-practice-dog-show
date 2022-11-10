e7c3845d011b78ec4554765d747d979373972cee
Mark Gowen <mark.gowen78@gmail.com>
2022-11-09 16:20:50 -0800

    edit button populates form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function init() {
-
     fetch('http://localhost:3000/dogs')
     .then(res => res.json())
     .then(function(data) {
@@ -9,16 +8,30 @@ function init() {
             const dogName = document.createElement("td")
             const dogBreed = document.createElement("td")
             const dogSex = document.createElement("td")
+            const button = document.createElement("td")
             const editBtn = document.createElement("button")
-
+            
             dogName.textContent = element.name
             dogBreed.textContent = element.breed
             dogSex.textContent = element.sex
             editBtn.textContent = "Edit Dog"
-
+            
+            button.append(editBtn)
             dog.append(dogName, dogBreed, dogSex, editBtn)
             table.append(dog)
+            editDog(element, editBtn)
         })
+    })
+}
+
+function editDog(element, editBtn) {
+    
+    editBtn.addEventListener("click", (e) => {
+        const form = document.querySelector("#dog-form")
+
+        form.name.value = element.name
+        form.breed.value = element.breed
+        form.sex.value = element.sex
     })
 }
 
